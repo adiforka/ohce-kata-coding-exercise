fa4cad28413712d05c0dfaf18239330958791bd7
adi forkasiewicz <adiforka@gmail.com>
2020-04-08 13:09:13 +0200

    slight refactong in getGreeting

--- src/main/java/OhceKata.java
+++ src/main/java/OhceKata.java
@@ -6,8 +6,7 @@ public class OhceKata {
     public String getGreeting(String name, int hour) {
 
         if (hour < 0 || hour > 23) throw new IllegalArgumentException("Hour must be between 0 and 23");
-
-        if (hour > 6 && hour < 12) return "¡Buenas dias, " + name + "!";
+        else if (hour > 6 && hour < 12) return "¡Buenas dias, " + name + "!";
         else if (hour >= 12 && hour < 20) return "¡Buenas tardes, " + name + "!";
         else return "¡Buenas nochas, " + name + "!";
     }
